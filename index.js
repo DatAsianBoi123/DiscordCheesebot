6921d63b5848fda811ada2c01de9ea488a3bbba2
DatAsianBoi123 <55264711+DatAsianBoi123@users.noreply.github.com>
2020-11-01 20:25:02 -0700

    f

--- index.js
+++ index.js
@@ -271,7 +271,7 @@ client.on('message', async message => {
 
       let embedVerification = new Discord.MessageEmbed()
         .setTitle('Verification')
-        .setDescription(`${message.user.name} is now verified as ${args[0]}!`)
+        .setDescription(`${message.users.name} is now verified as ${args[0]}!`)
         .setColor('PURPLE');
       const verifyRole = message.guild.roles.cache.find(role => role.name === 'VERIFIED');
       message.member.roles.add(verifyRole).catch(err => {
