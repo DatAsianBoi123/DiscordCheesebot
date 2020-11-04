8f2271c05255c6cbb3021839526af3a3495753fd
DatAsianBoi123 <55264711+DatAsianBoi123@users.noreply.github.com>
2020-11-03 17:15:04 -0700

    a

--- index.js
+++ index.js
@@ -344,9 +344,9 @@ client.on('message', async message => {
       textList.text = args[0];
       stringifyTextList = JSON.stringify(textList);
       fs.writeFile('./data.json', JSON.stringify(textList), finish);
-      console.log(`${JSON.stringify(textList)} / ${JSON.stringify(data)}, ${textList.text}`);
+      console.log(`${JSON.stringify(textList)} / ${JSON.parse(data)}, ${parseData.text}`);
 
-      message.channel.send(`Changed text to ${args[0]} (${JSON.stringify(textList)}, ${JSON.stringify(data)})!`);
+      message.channel.send(`Changed text to ${args[0]} (${JSON.stringify(textList)}, ${JSON.parse(data)})!`);
 
       break;
     }
