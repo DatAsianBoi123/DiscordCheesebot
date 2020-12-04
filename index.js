20bd0de3148f605cbc2aa429ae91f4b721752a31
DatAsianBoi123 <55264711+DatAsianBoi123@users.noreply.github.com>
2020-12-03 19:21:49 -0700

    a

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
 const fs = require('fs');
-const skillxp = require('skillxp.js');
+const skillxp = require('./skillxp.js');
 const prefix = 'b.';
 const client = new Discord.Client();
 
