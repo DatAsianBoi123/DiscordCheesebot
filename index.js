0b7cd7f6175e7a88a76a27ff2efbd926ebbcdb79
DatAsianBoi123 <55264711+DatAsianBoi123@users.noreply.github.com>
2020-12-03 17:00:25 -0700

    Modified command

--- index.js
+++ index.js
@@ -381,7 +381,7 @@ client.on('message', async message => {
       for (let i = 0; i < skyblockData.profiles.length; i ++) {
         if (skyblockData.profiles[i].cute_name.toLowerCase() == args[1].toLowerCase()) {
           console.log(skyblockData.profiles[i].members[accountData.id]);
-          message.channel.send(`${getLevelByXp(skyblockData.profiles[i].members[accountData.id].experience_skill_combat)} combat level`);
+          message.channel.send(`${getLevelByXp(skyblockData.profiles[i].members[accountData.id].experience_skill_combat).level} combat level`);
           return;
         }
       }
