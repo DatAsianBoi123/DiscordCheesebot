ec5a4c543580fa37293ba10cc52206fbfa0b7e39
Datasianboi123 <zach.su158@gmail.com>
2021-02-02 16:34:50 -0700

    Fixed Bug

--- commands/help.js
+++ commands/help.js
@@ -5,14 +5,13 @@ module.exports = {
   category: 'General',
   async execute(message, args) {
     const Discord = require('discord.js');
-    const client = new Discord.Client();
     const fs = require('fs');
     const index = require('../index');
 
     let allCommands = {}
     const commandFiles = fs.readdirSync('..').filter(file => file.endsWith('.js'));
     for (const files of commandFiles) {
-      const command = require(`../commands/${files}`);
+      const command = require(`./${files}`);
 
       allCommands[command.name] = command;
     }
