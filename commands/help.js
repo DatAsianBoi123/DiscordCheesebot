1c6054256b40f1c4abcb1791647e5e79bff12524
Datasianboi123 <zach.su158@gmail.com>
2021-02-02 19:09:38 -0700

    Added Debug

--- commands/help.js
+++ commands/help.js
@@ -28,6 +28,8 @@ module.exports = {
       color: 15105570,
       fields: []
     }
+    console.log(parseInt(args[0]));
+    console.log(index.categories[parseInt(args[0])]);
 
     for (const command in allCommands) {
       if (allCommands[command].category != index.categories[parseInt(args[0])]) return;
