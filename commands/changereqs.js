8f1dfb665952fb5c2177835414b1bc1383116c0c
Datasianboi123 <zach.su158@gmail.com>
2021-02-03 17:41:39 -0700

    Added Debug

--- commands/changereqs.js
+++ commands/changereqs.js
@@ -8,6 +8,7 @@ module.exports = {
     const index = require('../index');
 
     req = await index.getDataByType('Reqs', 'reqs');
+    console.log(`${args[0]}, ${req._id.toString()}`);
     index.updateById(req._id, 'reqs', args[0]);
     message.channel.send(req.reqs);
   }
