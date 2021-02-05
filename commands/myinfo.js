f5c93b63b1dff5e9e1506a8aabd0b5c802a6b02c
Datasianboi123 <zach.su158@gmail.com>
2021-02-05 11:44:34 -0700

    Fixed Bug

--- commands/myinfo.js
+++ commands/myinfo.js
@@ -9,21 +9,20 @@ module.exports = {
     const index = require('../index');
 
     if (!message.mentions.users.size && args[0]) {
-      console.log('ID');
-      /*client.users.fetch(args[0]).then((user) => {
-        return message.channel.send(args[0]);
+      client.users.fetch(args[0]).then((user) => {
+        return message.channel.send(user.username);
       }).catch(() => {
         return message.reply('Couldn\'t find a discord account with that id!');
-      });*/
+      });
     } else if (!message.mentions.users.size) {
       let personName = message.member.displayName;
       let personID = message.member.id;
       return message.channel.send(`You're name is ${personName}. \nYou're ID is ${personID}.`);
+    } else {
+      const personInfoList = message.mentions.users.map(user => {
+        return `${user.username}'s ID is ${user.id}`;
+      });
+      message.channel.send(personInfoList);  
     }
-
-    const personInfoList = message.mentions.users.map(user => {
-      return `${user.username}'s ID is ${user.id}`;
-    });
-    message.channel.send(personInfoList);
   }
 }
