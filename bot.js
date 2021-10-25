e71493a5fbaa22b4679434700c912f0d9f6faca9
DatAsianBoi123 <zach.su158@gmail.com>
2021-10-24 18:47:16 -0600

    a

--- bot.js
+++ bot.js
@@ -1,1 +1,9 @@
-console.log('Done');
+const { Intents, Client } = require('discord.js/src/index.js');
+
+const client = new Client({ intents: [Intents] })
+
+client.on('ready', () => {
+  console.log('Ready');
+});
+
+client.login(process.env.token);
