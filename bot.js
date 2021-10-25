3f16eb61bb42f3b3f1df054a7f6a1ff0f4adf299
DatAsianBoi123 <zach.su158@gmail.com>
2021-10-24 18:29:37 -0600

    Changed to a CommonJS Module

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-import { Client, Intents } from 'discord.js'
+const { Client, Intents } = require('discord.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
