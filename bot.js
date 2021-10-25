c8560970a2daaa6ecf4bbfff929a3dd05a59d9e9
DatAsianBoi123 <zach.su158@gmail.com>
2021-10-24 18:42:23 -0600

    x

--- bot.js
+++ bot.js
@@ -1,9 +1,1 @@
-const { Client, Intents } = require('discord.js');
-
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-
-client.on('ready', () => {
-  console.log('Ready.');
-});
-
-client.login(process.env.token);
+console.log('Done');
