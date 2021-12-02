2fb3ab5196a60f0e5906cde8a49aaa2b61ad5ab8
DatAsianBoi123 <zach.su158@gmail.com>
2021-12-01 17:00:12 -0700

    Added API_KEY field

--- config.ts
+++ config.ts
@@ -1,3 +1,4 @@
 export const TOKEN = process.env.token;
-export const CLIENT_ID = '749459685882593301';
+export const CLIENT_ID = process.env.client_id;
 export const GUILD_ID = '738961884853829703';
+export const API_KEY = process.env.apikey;
