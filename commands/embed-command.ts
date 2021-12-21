936d323a3a07a5a2e68fed0fca83b43c2ca66460
DatAsianBoi123 <zach.su158@gmail.com>
2021-12-20 21:33:12 -0700

    Added more

--- commands/embed-command.ts
+++ commands/embed-command.ts
@@ -11,16 +11,34 @@ module.exports = {
     .addSubcommand(subcommand => {
       return subcommand.setName('create')
         .setDescription('Creates a new message embed');
+    })
+    .addSubcommand(subcommand => {
+      return subcommand.setName('send')
+        .setDescription('Sends a message embed')
+        .addNumberOption(option => {
+          return option.setName('id')
+            .setDescription('The id of the message embed');
+        });
     }),
 
   type: 'GUILD',
 
-  callback: async ({ interaction }) => {
+  callback: async ({ interaction, args, channel }) => {
     if (!interaction.memberPermissions?.has('MANAGE_MESSAGES')) return interaction.reply({ content: 'You do not have permission to use this command', ephemeral: true });
 
-    const id = allEmbeds.size + 1;
-    allEmbeds.set(id, new MessageEmbed());
+    const id = args.getNumber('id');
 
-    interaction.reply({ content: `Created a new embed with the ID of ${id}`, ephemeral: true });
+    switch (args.getSubcommand()) {
+    case 'create':
+      const newId = allEmbeds.size + 1;
+      allEmbeds.set(newId, new MessageEmbed());
+
+      interaction.reply({ content: `Created a new embed with the ID of ${newId}`, ephemeral: true });
+
+      break;
+
+    case 'send':
+      channel.send({ embeds: [allEmbeds.get(id)] });
+    }
   },
 } as ICommand;
