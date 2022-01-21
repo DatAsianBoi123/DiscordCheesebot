866803f3ce62a1a85f6239066dbff26993003c7f
DatAsianBoi123 <zach.su158@gmail.com>
2022-01-20 20:20:54 -0700

    Added timestamps

--- models/account-links-model.ts
+++ models/account-links-model.ts
@@ -8,7 +8,7 @@ export interface IAccountLinkModel {
 const schema = new Schema<IAccountLinkModel>({
   minecraft_uuid: { type: String, required: true },
   discord_id: { type: String, required: true },
-});
+}, { timestamps: true });
 
 export default {
   model: model<IAccountLinkModel>('AccountLink', schema),
