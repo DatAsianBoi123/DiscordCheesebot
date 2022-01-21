1edad1a6306f6a788aa0933aeab8378198a6d300
DatAsianBoi123 <zach.su158@gmail.com>
2022-01-20 20:20:35 -0700

    Added all functionality

--- commands/link-command.ts
+++ commands/link-command.ts
@@ -1,4 +1,6 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
+import { MessageEmbed } from 'discord.js';
+import { API_KEY } from '../config';
 import accountLinksModel from '../models/account-links-model';
 import { ICommand } from '../typings';
 
@@ -18,12 +20,54 @@ module.exports = {
     onExecute: async ({ interaction, user, args }) => {
       await interaction.deferReply({ ephemeral: true });
 
-      await accountLinksModel.model.create({
-        discord_id: user.id,
-        minecraft_uuid: args.getString('name'),
-      });
+      const mojangData = await fetch(`https://api.mojang.com/users/profiles/minecraft/${args.getString('name')}`);
 
-      interaction.editReply('Done');
+      if (mojangData.status === 204) {
+        await interaction.editReply(`The player ${args.getString('name')} was not found`);
+
+        return;
+      }
+
+      const mojangJSON = await mojangData.json();
+
+      const hypixelData = await fetch(`https://api.hypixel.net/player?uuid=${mojangJSON.id}&key=${API_KEY}`);
+      const hypixelJSON = await hypixelData.json();
+
+      if (!hypixelJSON.success) {
+        const errorEmbed = new MessageEmbed()
+          .setTitle('Error')
+          .setDescription(`Code: **${hypixelData.status}**\nReason: ${hypixelJSON.cause}`)
+          .setColor('RED');
+
+        await interaction.editReply({ embeds: [errorEmbed] });
+
+        return;
+      }
+
+      if (!hypixelJSON.player) {
+        await interaction.editReply('This player has never joined hypixel');
+
+        return;
+      }
+
+      if (hypixelJSON.player.socialMedia.links.DISCORD !== user.tag) {
+        await interaction.editReply('Your discord account is not linked with your hypixel account!');
+
+        return;
+      }
+
+      await accountLinksModel.model.updateOne({ discord_id: user.id }, {
+        discord_id: user.id,
+        minecraft_uuid: mojangJSON.id,
+      }, { upsert: true });
+
+      const embed = new MessageEmbed()
+        .setTitle('Success!')
+        .setDescription(`Successfully linked your discord account with the minecraft account ${mojangJSON.name}`)
+        .setTimestamp()
+        .setColor('GREEN');
+
+      interaction.editReply({ embeds: [embed] });
     },
     onError: async ({ interaction, error }) => {
       console.log('An error occurred when executing the link command');
