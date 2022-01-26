90afea90dddf2c455bb500b48432041340e303d2
DatAsianBoi123 <zach.su158@gmail.com>
2022-01-25 21:21:44 -0700

    Added set subcommand

--- commands/burgis-bucks-command.ts
+++ commands/burgis-bucks-command.ts
@@ -14,9 +14,24 @@ module.exports = {
             .setDescription('The user the command is directed towards')
             .setRequired(false);
         });
+    })
+    .addSubcommand(subcommand => {
+      return subcommand.setName('set')
+        .setDescription('Sets a user\'s burgis bucks to an amount')
+        .addUserOption(option => {
+          return option.setName('user')
+            .setDescription('The user the command is directed towards')
+            .setRequired(true);
+        })
+        .addIntegerOption(option => {
+          return option.setName('amount')
+            .setDescription('The amount to set by')
+            .setRequired(true);
+        });
     }),
 
   type: 'GUILD',
+  disallowedTextChannels: ['DM'],
 
   listeners: {
     onExecute: async ({ interaction, args, user: author }) => {
@@ -36,6 +51,17 @@ module.exports = {
         }
 
         interaction.editReply(`${user.username} has ${model.balance} burgis bucks`);
+
+        break;
+
+      case 'set':
+        if (!interaction.memberPermissions?.has('MANAGE_GUILD')) return interaction.editReply('You do not have permission to use this command');
+
+        const amount = args.getInteger('amount');
+
+        await burgisBuckModel.model.updateOne({ id: user.id }, { $set: { balance: amount } }, { upsert: true });
+
+        interaction.editReply(`Successfully set ${user.username}'s burgis bucks to ${amount}`);
 
         break;
       }
